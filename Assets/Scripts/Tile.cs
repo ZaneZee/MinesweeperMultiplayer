5d6927ade350e77cb6b18777b41f478d3bac5d44
ZaneZee <zanezee@gmail.com>
2019-09-06 12:32:24 -0700

    Test commit
    testing in tile script

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour {
-    //kappa
+    //something else entirely
     #region Variable Declarations
 
     private Board board;
